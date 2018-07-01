8cf946933b87fd17e7a2d95a4c8c8a4d33fb39b3
nirmaladvani <nirmaladvani@live.com>
2018-07-01 15:07:16 -0700

    Task 01 and 02

---
+++ Task 01/code.js
@@ -0,0 +1,10 @@
+function pop(){
+    var tex = document.getElementById("heading1"); //get element with id specified
+    if (tex.style.display ==="none"){
+        tex.style.display= "block"; //text becomes visible
+    }
+    else{
+        tex.style.display = "none"; //text hides
+    }
+
+}
---
+++ Task 02/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+var app = express();
+app.use(bodyparser.urlencoded({ extended: false}));
+
+app.get('/', function(req,res){
+    res.sendFile("C:/Users/Haier/Desktop/test/index.html");
+});
+
+app.post('/login', function(req,res){
+    var user_name=req.body.user;
+    var password=req.body.password;
+    console.log("From client pOST request: User ="+user_name+" and password is " + password);
+    res.end("yes");
+});
+
+app.listen(3000, function(){
+    console.log("Started on PORT 3000")
+})
